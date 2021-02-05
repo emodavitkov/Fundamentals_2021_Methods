321cefc393dc846987007f8a04fbd79cf972ae63
emodavitkov <emodavitkov@abv.bg>
2021-02-05 12:20:46 +0200

    Add project files.

---
+++ Fundamentals_2021_Methods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SignofIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            //SignChecker(num);
+            string result = SignChecker(num);
+            Console.WriteLine(result);
+
+            //SignChecker(int.Parse(Console.ReadLine()));
+        }
+
+        static string SignChecker(int number)
+        {
+            string sign = null;
+
+            if (number>0)
+            {
+                sign = "positive";
+            }
+
+            else if (number<0)
+            {
+                sign = "negative";
+            }
+
+            else
+            {
+                sign = "zero";
+            }
+            return $"The number {number} is {sign}.";
+
+            //static void SignChecker(int number)
+            //string sign = null;
+
+            //if (number > 0)
+            //{
+            //    sign = "positive";
+            //}
+
+            //else if (number < 0)
+            //{
+            //    sign = "negative";
+            //}
+
+            //else
+            //{
+            //    sign = "zero";
+            //}
+            //Console.WriteLine($"The number {number} is {sign}.");
+        }
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grades(double.Parse(Console.ReadLine()));
+        }
+
+
+        static void Grades(double grade)
+        {
+            string gradeInWords = String.Empty;
+
+            if (grade>=2 &&grade<=2.99)
+            {
+                gradeInWords = "Fail";
+            }
+
+            else if (grade>=3 && grade<=3.49)
+            {
+                gradeInWords = "Poor";
+            }
+
+            else if (grade >= 3.5 && grade <= 4.49)
+            {
+                gradeInWords = "Good";
+            }
+
+            else if (grade >= 4.5 && grade <= 5.49)
+            {
+                gradeInWords = "Very good";
+            }
+
+            else if (grade >= 5.5 && grade <= 6.00)
+            {
+                gradeInWords = "Excellent";
+            }
+
+            Console.WriteLine(gradeInWords);
+
+        }
+    }
+}
